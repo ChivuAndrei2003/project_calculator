2cde54aefec57571d5c0661d35cae0981bb63a98
Chivu Andrei <chivuandrei2003@gmail.com>
2025-03-02 17:18:42 +0200

    I've added the frontend interface

--- script.js
+++ script.js
@@ -1,43 +1,60 @@
-let userChoice;
-let num1;
-let num2;
-let operator;  
-//cred ca trebuie sa fac ceva cu arrays la num1,num2 operattor (balari)
-// care sa ia valorile introduse de utilizator
-//ca design vreau sa fie un iphone cu aplicatia calculator deschisa
-// cu btn on click nu trebuie sa faci aia cu parse int sdau prompt
-//pt a lau valorile utilizatorului
-function Clear(){}
-
-function Operate(num1,num2,operator){
-    
-    if (operator==='+'){
-        Add(num1,num2);
-    };
-    else if(operator==='*'){
-        Multiply(num1,num2);
-    };
-    else if(operator==='-'){
-        Subtract(num1,num2);
-    };
-    else{Divide(num1,num2);
-    };
-
-}
-
-
 function Add(a,b){
-    return a+b;
+    return a + b;
 };
 
 function Subtract(a,b){
-    return a-b;
+    return a - b;
 };
-
 function Multiply(a,b){
-    return a*b;
+    return a * b;
+};
+function Divide(a,b){
+    return a / b;
 };
 
-function Divide(a,b){
-    return a/b;
+
+let num1;
+let num2;
+let operator;
+
+function Operate(num1,num2,operator){
+   let result;
+    switch(operator){
+        case "+":
+            result = Add(num1,num2) ;
+            break ;
+        
+        case "-":
+            result = Subtract(num1,num2);
+            break;
+        
+        case "*":
+            result = Multiply(num1,num2);
+            break ;
+
+        case "/":
+            if (num2 !== 0){
+                result = Divide(num1,num2);}
+                else {
+                    result="lmao";
+                }
+            break;
+       
+            
+
+
+    }
+    return result;
 };
+//I have to continue tomorrow bcs I'm too tired right now
+
+// buttons is a node list. It looks and acts much like an array.
+const buttons = document.querySelectorAll("button");
+
+// we use the .forEach method to iterate through each button
+buttons.forEach((button) => {
+  // and for each one we add a 'click' listener
+  button.addEventListener("click", () => {
+    alert(button.textContent);
+  });
+});
